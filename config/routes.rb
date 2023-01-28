a3a7d5132a64b3efeaa3c5bd15c8e64f9f6639e0
dsitweeed <luffy3042001@gmail.com>
2023-01-28 22:31:56 +0700

    Add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_page/home'
   devise_for :users
-  devise_for :models
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root "static_pages#home"
 end
